9267d1cb25f8d1f2d4dcb2dfbe5e83f19f756825
James Till Matta <jamesmatta@gmail.com>
2016-12-28 19:01:10 -0500

    Reset digitizer between tests
    Signed-Off-By: James Till Matta <jamesmatta@gmail.com>

--- testRunner.sh
+++ testRunner.sh
@@ -2,6 +2,7 @@ git checkout dirtyOldDigitizer
 make release
 ./efftest orchid_cfg < runcmds.txt
 make superclean
+/home/prospect/DigitizerTester/digitizerClearer
 git checkout newDigitizerReading
 make release
 ./efftest orchid_cfg < runcmds.txt
