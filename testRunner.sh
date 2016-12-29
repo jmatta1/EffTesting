c15fcb50232baa87dff4e339f6869ed343d114ea
James Till Matta <jamesmatta@gmail.com>
2016-12-28 19:24:17 -0500

    Tweak to testing
    Signed-Off-By: James Till Matta <jamesmatta@gmail.com>

--- testRunner.sh
+++ testRunner.sh
@@ -1,7 +1,6 @@
 git checkout dirtyOldDigitizer
 make release
 ./efftest orchid_cfg < runcmds.txt
-make superclean
 /home/prospect/DigitizerTester/digitizerClearer
 git checkout newDigitizerReading
 make release
